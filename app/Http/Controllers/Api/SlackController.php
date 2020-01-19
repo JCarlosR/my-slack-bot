094d20f3004c00c8c0871dad9365d53ae34e3400
Juan Ramos <juancagb.17@gmail.com>
2020-01-19 11:10:12 -0500

    Fix namespace

--- app/Http/Controllers/Api/SlackController.php
+++ app/Http/Controllers/Api/SlackController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Slack\ApiClient;
 use App\SlackEvent;
 use Illuminate\Http\Request;
